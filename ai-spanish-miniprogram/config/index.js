4aa9c5bc25e4cdc503a1776964aef6c8c246f5c6
heavenmei <1394003825@qq.com>
2025-01-16 13:09:02 +0800

    1.1.1

--- ai-spanish-miniprogram/config/index.js
+++ ai-spanish-miniprogram/config/index.js
@@ -6,8 +6,8 @@ export function getEnv() {
   return envVersion;
 }
 
-// const dev = "http://127.0.0.1:8000";
-const dev = "http://192.168.1.103:8000";
+const dev = "http://127.0.0.1:8000";
+// const dev = "http://192.168.1.103:8000";
 
 const pro = "https://ai-spanish.cn";
 
